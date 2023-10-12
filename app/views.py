168cef6e8808bcb2b058d69a5dddd6722e5d551a
abishekvp <abisihek.vp13@gmail.com>
2023-10-13 01:55:13 +0530

    update

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.contrib.auth.models import User
 
 def index(request):
@@ -16,10 +17,15 @@ def signup(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        user = User.objects.create_user(username, email, password)
-        user = authenticate(request, username=username, password=password)
-        login(request, user)
-        return redirect("dashboard")
+        if User.objects.filter(username=username).exists() and User.objects.filter(email=email).exists():messages.info(request, 'Username and Email already exists')
+        elif User.objects.filter(username=username).exists():messages.info(request, 'Username already exists')
+        elif User.objects.filter(email=email).exists():messages.info(request, 'Email already exists')
+        else:
+            user = User.objects.create_user(username, email, password)
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect("dashboard")
+        return redirect("signup")
     else:return render(request,'signup.html')
 
 def signin(request):
@@ -28,11 +34,11 @@ def signin(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-        login(request, user)
+        if user:login(request, user)
+        else:messages.info(request, 'User not found')
         return redirect("dashboard")
     else:return render(request,'signin.html')
 
 def signout(request):
-    if request.user.is_authenticated:
-        logout(request)
+    if request.user.is_authenticated:logout(request)
     return redirect('signin')
